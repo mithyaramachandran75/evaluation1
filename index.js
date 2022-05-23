e964c086f910b944ea39ff0bda07c7661c4c1e30
mithyaramachandran75 <104683345+mithyaramachandran75@users.noreply.github.com>
2022-05-23 17:33:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//store the products array in localstorage as "products"
+function Product(t, d, p, i){
+    this.type=t;
+    this.desc=d;
+    this.price=p;
+    this.image=i;
+}
+
+function storeData(e){
+   e.preventDefault();  
+let form= document.getElementById("products");
+let type=form.type.value;
+let desc=form.desc.value;
+let price=form.price.value;
+let image=form.image.value;
+
+let p= new Product(type, desc, price, image)
+
+let data= JSON.parse(localStorage.getItem("products")) || [];
+
+data.push(p)
+localStorage.setItem("products",JSON.stringify(data));
+
+console.log(p)
+}
